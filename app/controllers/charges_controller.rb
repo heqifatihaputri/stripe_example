b3f6bb93064c690af0ef9955a7b7001a730b68f5
3lviend <henryss_10@yahoo.com>
2019-01-02 13:21:23 +0700

    first commit

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,39 @@
+class ChargesController < ApplicationController
+  Stripe.api_key = Rails.application.secrets.stripe_secret_key
+  before_action :authenticate_user!
+  # before_action :find_product
+
+  def new
+  end
+
+  def create
+    debugger
+    #Amont in cents
+    # @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer     => customer.id,
+      :amount       => @product.price,
+      :description  => 'Rails Stripe customer',
+      :currency     => 'usd'
+    )
+
+    redirect_to root_path
+
+    rescue Stripe::CardError => e
+      flash[:error] = e.message
+      redirect_to root_path
+  end
+  
+  def find_product
+    @product = Product.find(params[:id])
+    # rescue ActiveRecord::RecordNotFound => e
+    #   flash[:error] = 'Product not found!'
+    # end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # has_many :credit_cards, dependent: :destroy
+ 
+  # after_commit :assign_customer_id, on: :create
+ 
+  # def assign_customer_id
+  #   customer = Stripe::Customer.create(email: email)
+  #   self.customer_id = customer.id
+  # end
+end
